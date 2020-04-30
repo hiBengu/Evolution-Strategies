f2d3fa8b1396ee7d3610eec744d83840d8c493db
hibengu <hibrahimbengu@gmail.com>
2020-04-30 04:42:41 +0300

    Fix movement bugs

--- pacmanPygame.py
+++ pacmanPygame.py
@@ -5,38 +5,50 @@ class pacman(object):
     def __init__(self, pos):
         self.pos = pos
         self.color = (0,255,255)
-        self.rect = pygame.Rect(self.pos[0]*gridSize, self.pos[1]*gridSize, gridSize / 2, gridSize / 2)
+        self.rect = pygame.Rect(self.pos[0]*gridSize, self.pos[1]*gridSize, gridSize , gridSize )
         self.speed = 5
         self.dir = (1,0)
         self.prevDir = (0,0)
+        self.turnTry = 0
+        self.desiredDir = (1,1)
 
     def move(self):
         events = pygame.event.get()
 
+        # Takes the input from keyboard and creates a tuple for the input direction
         for event in events:
             if event.type == pygame.KEYDOWN:
-                print('bastin')
                 if event.key == pygame.K_UP:
-                    self.prevDir = self.dir
                     self.dir = (0,-1)
                 elif event.key == pygame.K_DOWN:
-                    self.prevDir = self.dir
                     self.dir = (0,1)
                 elif event.key == pygame.K_RIGHT:
-                    self.prevDir = self.dir
                     self.dir = (1,0)
                 elif event.key == pygame.K_LEFT:
-                    self.prevDir = self.dir
                     self.dir = (-1,0)
 
+        # Controls if there will be a collision if player moves to desired direction
         if self.checkCollision(self.dir):
+            self.desiredDir = self.dir
+            self.turnTry = 5 # A variable to turn to the wanted direction if it is close
             if not self.checkCollision(self.prevDir):
                 self.dir = self.prevDir
             else:
                 self.dir = (0,0)
 
+        # Tries to turn to old input until turnTry = 0
+        if self.turnTry != 0:
+            if not self.checkCollision(self.desiredDir):
+                self.rect.left = self.rect.left + self.desiredDir[0] * self.speed
+                self.rect.top = self.rect.top + self.desiredDir[1] * self.speed
+                self.dir = self.desiredDir
+                self.turnTry = 0
+                return True
+            self.turnTry = self.turnTry - 1
+
         self.rect.left = self.rect.left + self.dir[0] * self.speed
         self.rect.top = self.rect.top + self.dir[1] * self.speed
+        self.prevDir = self.dir
 
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect)
@@ -48,6 +60,10 @@ class pacman(object):
         return testRect.collidelistall(wallObjects)
 
 class monsterStruct(object):
+    '''
+    Monster objects are created like player object.
+    Monster's direction is changed whenever it hit something
+    '''
     def __init__(self, pos):
         self.pos = pos
         self.color = (255, 122, 0)
@@ -80,6 +96,7 @@ class wallStruct(object):
         pygame.draw.rect(screen, self.color, self.rect)
 
 class foodStruct(object):
+    '''A rectangle object with half of the gridsize and centered in the middle'''
     def __init__(self, pos):
         self.pos = pos
         self.size = gridSize / 2
@@ -89,6 +106,7 @@ class foodStruct(object):
         pygame.draw.rect(screen, self.color, self.rect)
 
 def checkGame():
+    '''Checks for global actions in game, such as score or death.'''
     global score, foodObjects
 
     if player.rect.collidelistall(foodObjects):
@@ -106,6 +124,7 @@ def drawMonsters():
         monster.draw()
 
 def initMonsters():
+    '''Monster objects are created from hard-coded coordinates'''
     global monsterObjects
 
     monsterCoords = [(15, 0), (4,8), (18,10), (3, 16), (2,6)]
@@ -116,6 +135,10 @@ def initMonsters():
         monsterObjects.append(monster)
 
 def initFoods(renew=False):
+    '''
+    If renew is True, all food coordinates are decided amongst the empty places
+    with 0.6 chance. After that all food objects are created as global.
+    '''
     global foodObjects, foodCoords
 
     if renew:
@@ -132,6 +155,10 @@ def initFoods(renew=False):
         foodObjects.append(food)
 
 def initWalls():
+    '''
+    Walls are hard-coded into the game. Depending on the coorddinates wall objects
+    are created as a global list
+    '''
     global wallObjects, wallCoords
 
     wallCoords = [
@@ -194,7 +221,7 @@ def main():
     initFoods(True)
 
     while True:
-        clock.tick(30)
+        clock.tick(30) # 30 fps
         updateScreen()
 
 if __name__ == "__main__":
